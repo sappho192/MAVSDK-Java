38e894d48a4d9a6f92542f6aeb0641b7fa231a25
TAE-IN Sappho KIM <sappho192@gmail.com>
2023-05-18 10:09:41 +0900

    Add mission execution code in RunMission.java

--- examples/java-client/src/main/java/io/mavsdk/example/RunMission.java
+++ examples/java-client/src/main/java/io/mavsdk/example/RunMission.java
@@ -1,12 +1,20 @@
+/* I've changed official RunMission.java in MAVSDK-Java (v1.3.1) example a bit
+*  because the example is just uploading & downloading mission.
+*  The same example in C++ SDK demonstrates not only uploading but executing mission so I applied similar logic to this Java example code.
+*/
+
 package io.mavsdk.example;
 
 import io.mavsdk.System;
 import io.mavsdk.mission.Mission;
+import io.mavsdk.telemetry.Telemetry;
 import io.reactivex.CompletableSource;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -16,20 +24,56 @@ public class RunMission {
   public static void main(String[] args) {
     logger.debug("Starting example: mission...");
 
-    System drone = new System();
+    string SERVER_IP = "172.0.0.1";
+    int SERVER_PORT = 50051;
+    System drone = new System(SERVER_IP, SERVER_PORT);
 
     drone.getTelemetry().getFlightMode()
-        .doOnNext(flightMode -> logger.debug("flight mode: " + flightMode))
-        .subscribe();
+            .doOnNext(flightMode -> {
+              logger.debug("flight mode: " + flightMode);
+              var position = drone.getTelemetry().getPosition().blockingFirst();
+              logger.debug(String.format("Position: Altitude %f / Latitude %f / Longitude %f",
+                      position.getAbsoluteAltitudeM(), position.getLatitudeDeg(), position.getLongitudeDeg()));
+            })
+            .subscribe();
+    drone.getTelemetry().getArmed()
+            .doOnNext(armed -> {
+              if(!armed) {
+                logger.debug("Disarmed");
+              }
+            })
+            .subscribe();
+    drone.getTelemetry().getLandedState()
+            .doOnNext(landed -> {
+              switch (landed) {
+                case UNKNOWN:
+                  break;
+                case ON_GROUND:
+                  logger.debug("Drone: ON_GROUND");
+                  break;
+                case TAKING_OFF:
+                  logger.debug("Drone: TAKING_OFF");
+                  break;
+                case LANDING:
+                  logger.debug("Drone: LANDING");
+                  break;
+              }
+            })
+            .subscribe();
+//    drone.getTelemetry().getPosition()
+//            .doOnNext(position -> logger.debug(String.format("Alt: %s, Long: %f, Lat: %f", position.getAbsoluteAltitudeM(), position.getLongitudeDeg(), position.getLatitudeDeg())))
+//            .subscribe();
 
     List<Mission.MissionItem> cycle = new ArrayList<>();
-    cycle.add(generateMissionItem(47.398039859999997, 8.5455725400000002));
-    cycle.add(generateMissionItem(47.398036222362471, 8.5450146439425509));
-    cycle.add(generateMissionItem(47.397825620791885, 8.5450092830163271));
-    cycle.add(generateMissionItem(47.397832880000003, 8.5455939999999995));
+    cycle.add(generateMissionItem(47.398170327054473, 8.5456490218639658, 10.0f, 5.0f, false, 20.0f, 60.0f, Mission.MissionItem.CameraAction.NONE));
+    cycle.add(generateMissionItem(47.398241338125118, 8.5455360114574432, 10.0f, 2.0f, true, 0.0f, -60.0f, Mission.MissionItem.CameraAction.NONE));
+    cycle.add(generateMissionItem(47.398139363821485, 8.5453846156597137, 10.0f, 5.0f, true, -45.0f, 0.0f, Mission.MissionItem.CameraAction.NONE));
+    cycle.add(generateMissionItem(47.398058617228855, 8.5454618036746979, 10.0f, 2.0f, false, -90.0f, 30.0f, Mission.MissionItem.CameraAction.NONE));
+    cycle.add(generateMissionItem(47.398100366082858, 8.5456969141960144, 10.0f, 5.0f, false, -45.0f, -30.0f, Mission.MissionItem.CameraAction.NONE));
+    cycle.add(generateMissionItem(47.398001890458097, 8.545557618141174, 10.0f, 5.0f, false, 0.0f, 0.0f, Mission.MissionItem.CameraAction.NONE));
 
     List<Mission.MissionItem> missionItems = new ArrayList<>();
-    for (int i = 0; i < 300; i++) {
+    for (int i = 0; i < 1; i++) {
       missionItems.addAll(cycle);
     }
 
@@ -38,15 +82,17 @@ public class RunMission {
 
     CountDownLatch latch = new CountDownLatch(1);
     drone.getMission()
-        .setReturnToLaunchAfterMission(true)
-        .andThen(drone.getMission().uploadMission(missionPlan)
-            .doOnComplete(() -> logger.debug("Upload succeeded")))
-        .andThen(drone.getMission().downloadMission()
-            .doOnSubscribe(disposable -> logger.debug("Downloading mission"))
-            .doAfterSuccess(disposable -> logger.debug("Mission downloaded")))
-        .toCompletable()
-        .andThen((CompletableSource) cs -> latch.countDown())
-        .subscribe();
+            .setReturnToLaunchAfterMission(true)
+            .andThen(drone.getMission().uploadMission(missionPlan)
+                    .doOnComplete(() -> logger.debug("Upload succeeded")))
+            .andThen(drone.getAction().arm().doOnComplete(() -> logger.debug("Armed")))
+            .andThen(drone.getMission().startMission().doOnComplete(() -> logger.debug("Starting mission...")))
+            .andThen(drone.getMission().downloadMission()
+                    .doOnSubscribe(disposable -> logger.debug("Downloading mission"))
+                    .doAfterSuccess(disposable -> logger.debug("Mission downloaded")))
+            .toCompletable()
+            .andThen((CompletableSource) cs -> latch.countDown())
+            .subscribe();
 
     try {
       latch.await();
@@ -55,21 +101,24 @@ public class RunMission {
     }
   }
 
-  public static Mission.MissionItem generateMissionItem(double latitudeDeg, double longitudeDeg) {
+  public static Mission.MissionItem generateMissionItem(
+          double latitudeDeg, double longitudeDeg,
+          float relativeAltitudeM, float speedMSS, boolean isFlyThrough,
+          float gimbalPitchDeg, float gimbalYawDeg,
+          Mission.MissionItem.CameraAction cameraAction) {
     return new Mission.MissionItem(
-        latitudeDeg,
-        longitudeDeg,
-        10f,
-        10f,
-        true,
-        Float.NaN,
-        Float.NaN,
-        Mission.MissionItem.CameraAction.NONE,
-        Float.NaN,
-        Double.NaN,
-        Float.NaN,
-        Float.NaN,
-        Float.NaN);
+            latitudeDeg,
+            longitudeDeg,
+            relativeAltitudeM,
+            speedMSS,
+            isFlyThrough,
+            gimbalPitchDeg,
+            gimbalYawDeg,
+            cameraAction,
+            Float.NaN,
+            Double.NaN,
+            Float.NaN,
+            Float.NaN,
+            Float.NaN);
   }
 }
-
